e85589fbd661b09133bbc272165e5faba69e9b1c
cancerous pmmp spoons <64ff00@gmail.com>
2017-08-08 19:06:29 +0900

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+from cancer.meme import sauce
+
+import logging
+
+l=logging.getLogger()
+l.setLevel(logging.WARNING)
+
+
+
+
+def garbage():
+    l.warning('some nice sample code')
+    
+    
+garbage()
+
+
+
+
+
+
+
+
+
+# best fork 
+    
