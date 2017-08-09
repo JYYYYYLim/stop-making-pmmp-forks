17888a31a8b4014de0e7e509d7afdc28e58f6795
cancerous pmmp spoons <64ff00@gmail.com>
2017-08-09 19:21:05 +0900

    Create javasofast.java

---
+++ util/javasofast.java
@@ -0,0 +1,9 @@
+
+
+
+rlflleeflelffllrlrlrllreleeeeflellflflflfrlelee==
+1361929393293484848824881919129348920019383829119111991199383
+
+
+
+php> javascript > java > hpphphphphphpython > rust > c++ > c#
