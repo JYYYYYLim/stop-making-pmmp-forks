c226ec3a0619689f54a4d805cb8a2f29187b5415
Johnmacrocraft <Johnmacrocraft@users.noreply.github.com>
2017-08-22 16:59:25 +0900

    Create ProtocolInfo.js

---
+++ network/mcbr/protocol/ProtocolInfo.js
@@ -0,0 +1,13 @@
+javascript and java is same. - noob developer
+imma hack ur pc - script kiddie
+
+/**
+* NOTE TO DEVELOPERS
+* Waste your time or ours submitting pull requests changing game and/or protocol version numbers.
+* Pull requests changing game and/or protocol version numbers will be merged.
+*
+* This file is generated manually, edit it manually.
+*/
+
+const CURRENT_PROTOCOL = 0;
+const ACCEPTED_PROTOCOLS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50];
